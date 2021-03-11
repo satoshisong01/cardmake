bd407e820dea2d38c794b5a7f3142dcd5fb8014b
Sysner04 <sysner04@Sysner04ui-Macmini.local>
2021-03-11 13:45:11 +0900

    add CardAddForm component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import CardAddForm from '../card_add/card_add_form';
+import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = (props) => (
-<section className={styles.editor}>
+const Editor = ({ cards, addCard }) => (
+  <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
-</section>
+    {cards.map(card => (
+      <CardEditForm key={card.id} card={card} />
+    ))}
+    <CardAddForm onAdd={addCard}/>
+  </section>
 );
 
 export default Editor;
